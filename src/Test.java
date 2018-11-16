52f31c2ba0f454b08c2e64a5a54636657f08fdbe
129cjl <1874236224@qq.com>
2018-11-16 22:18:27 +0800

    多次输出

--- src/Test.java
+++ src/Test.java
@@ -6,5 +6,14 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("\"你好\" = " + "你好");
+        System.out.println("\"你好\" = " + "你好");
+
+        System.out.println("\"你好\" = " + "你好");
+        System.out.println("\"你好\" = " + "你好");
+
+        System.out.println("\"你好\" = " + "你好");
+
+
     }
+
 }
