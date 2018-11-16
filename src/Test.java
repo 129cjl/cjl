8931314ec365584685a15d99ea8852b79123cea9
129cjl <1874236224@qq.com>
2018-11-16 21:56:23 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @className: Test
+ * @author junliang-cjl 2018-11-16 21:54
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("\"ÄãºÃ\" = " + "ÄãºÃ");
+    }
+}
